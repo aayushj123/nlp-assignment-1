880e08df69298ad40c7fb147763d3aeef743b6e7
Aayush Jha <60804417+aayushj123@users.noreply.github.com>
2022-10-17 20:18:03 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,19 +1,15 @@
 
-# Creating corpus variable for 1st question
-corpus_q1 = corpus
-
-# Interesting regex patterns
 # Long words (longer than 15 characters)
-long_words = re.findall(r'\w{15,}', corpus_q1)
+long_words = re.findall(r'\w{15,}', corpus)
 
 # Words with hyphen
-hyphenated_words = re.findall(r'\w+-\w+', corpus_q1)
+hyphenated_words = re.findall(r'\w+-\w+', corpus)
 
 # Words ending with common morphological suffixes, also indicating verbs
-morphological_suffixes_words = re.findall(rf'\b\w+ed|\w+ing|\w+ly\b', corpus_q1)
+morphological_suffixes_words = re.findall(rf'\b\w+ed|\w+ing|\w+ly\b', corpus)
 
-# Patters that indicate proper nouns
-proper_nouns = re.findall(r'\b[A-Z][a-z]+\b', corpus_q1)
+# proper nouns
+proper_nouns = re.findall(r'\b[A-Z][a-z]+\b', corpus)
 
 print("\nLong words (longer than 15 characters): ", long_words)
 print("\nHyphenated words: ", hyphenated_words)
@@ -21,20 +17,20 @@ print("\nCommon morphological suffixes indicating verb: ", morphological_suffixe
 print("\nProper nouns: ", proper_nouns)
 
 # Clean the corpus
-# Remove all the punctuations
-corpus_q1 = re.sub(r'[^\w\s]', '', corpus_q1)
+# Remove punctuations
+corpus = re.sub(r'[^\w\s]', '', corpus)
 
-# Remove the numbers
-corpus_q1 = re.sub(r'\d+', '', corpus_q1)
+# Remove numbers
+corpus = re.sub(r'\d+', '', corpus)
 
 # Remove all the extra spaces
-corpus_q1 = re.sub(r'\s+', ' ', corpus_q1)
+corpus = re.sub(r'\s+', ' ', corpus)
 
-# Convert all the words to lower case
-corpus_q1 = corpus_q1.lower()
+# change all words to lower case
+corpus = corpus.lower()
 
 # Tokenize the corpus and mention the counts of top 10 words excluding stop words
-tokens = nltk.word_tokenize(corpus_q1)
+tokens = nltk.word_tokenize(corpus)
 tokens = [token for token in tokens if token not in stop_words]
 fdist = nltk.FreqDist(tokens)
 top_10 = fdist.most_common(10)
